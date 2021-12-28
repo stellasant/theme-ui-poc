9413c25265c512e9e6f9025a7d8e8abc94611987
Stella Santamaría <stella.m.santa@gmail.com>
2021-12-28 13:05:23 -0300

    remove component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import { ThemeProvider } from 'theme-ui'
 import { defaultTheme } from './ui/theme'
-import { Dialogs } from './components/Dialog'
 
 export const App = () => {
     return (
         <ThemeProvider theme={defaultTheme}>
-            {/* <Tooltip /> */}
-            {/* <Drawer /> */}
-            <Dialogs />
-            // Add Project main content
+          {/*Add Project main content*/}
         </ThemeProvider>
     )
 }
