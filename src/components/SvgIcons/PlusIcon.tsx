8a6fce9cdca1e406c243f5e728be46fcf201e9a8
msense <developer@vetsource.com>
2021-12-30 16:25:08 -0300

    Add Icon folder, IconButton and icon prop in Button

---
+++ src/components/SvgIcons/PlusIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PlusIcon = (): JSX.Element => (
+    <svg fill="currentcolor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path d="M24 9h-9v-9h-6v9h-9v6h9v9h6v-9h9z" />
+    </svg>
+)
